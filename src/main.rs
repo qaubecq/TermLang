4146585fabe46aef85d3bd53cb4d4e29eb6ceb09
Flambynou <tom.ghislain@ik.me>
2026-03-10 19:36:21 +0100

    render function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod renderer;
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/renderer.rs
@@ -0,0 +1,45 @@
+use std::io::{self, Write};
+use std::sync::Arc;
+use terminal_size::{Height, Width, terminal_size};
+
+fn render(sigma: Arc<Vec<Vec<(u8, u8, u8)>>>) -> std::io::Result<()> {
+    if sigma.is_empty() {
+        return Ok(());
+    };
+    if let Some((Width(w), Height(h))) = terminal_size() {
+        if w < sigma.len() as u16 || h < sigma[0].len() as u16 {
+            return Err(io::Error::new(
+                io::ErrorKind::OutOfMemory,
+                "Sigma is too big for the current terminal size",
+            ));
+        };
+    } else {
+        return Err(io::Error::other("Could not measure terminal"));
+    }
+
+    let mut stdout = std::io::stdout();
+    write!(stdout, "\x1B[H")?;
+    for lines in sigma.chunks(2) {
+        if lines.len() == 2 {
+            let upper = &lines[0];
+            let lower = &lines[1];
+            for (up, down) in upper.iter().zip(lower) {
+                write!(
+                    stdout,
+                    "\x1B[38;2;{};{};{}m\x1B[48;2;{};{};{}m▀",
+                    up.0, up.1, up.2, down.0, down.1, down.2,
+                )?;
+            }
+            write!(stdout, "\x1B[E")?;
+        } else {
+            write!(stdout, "\x1B[49m")?;
+            let line = &lines[0];
+            for up in line {
+                write!(stdout, "\x1B[38;2;{};{};{}m▀", up.0, up.1, up.2,)?;
+            }
+        }
+    }
+
+    stdout.flush()?;
+    Ok(())
+}
